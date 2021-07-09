dfd3f60cd5ddfc21f228a6691ad863adee51a83b
LuoMing <Indomite@qq.com>
2021-07-09 10:22:39 +0800

    Create leetcode49.js

---
+++ leetcode/leetcode49.js
@@ -0,0 +1,16 @@
+//字母异位符
+
+var groupAnagrams = function(strs) {
+  const map = new Map();
+  for (let str of strs) {
+      let array = Array.from(str);
+      array.sort();
+      let key = array.toString();
+      let list = map.get(key) ? map.get(key) : new Array();
+      list.push(str);
+      map.set(key, list);
+  }
+  return Array.from(map.values());
+};
+
+console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
