01152e8021957d57b4db3e16dd941902361d0c43
LuoMing <Indomite@qq.com>
2021-08-03 00:31:35 +0800

    Create myIntanceof.js

---
+++ Coding/myIntanceof.js
@@ -0,0 +1,14 @@
+function myInstanceof (left, right) {
+  // 获取对象的原型
+  let proto = Object.getPrototypeOf(left);
+  // 获取构造函数的 prototype 对象
+  let prototype = right.prototype;
+  console.log(prototype);
+  while(true) {
+    if(!proto) return false;
+    if(proto === prototype) return true;
+    proto = Object.getPrototypeOf(proto);
+  }
+}
+
+console.log(myInstanceof(1, Number));
