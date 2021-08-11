69321503161f6fe4ac031922cb9cd426c2ca0c5c
LuoMing <Indomite@qq.com>
2021-08-12 00:24:46 +0800

    feat: 8.12

---
+++ Algorithm/leetcode/53.最大子序和.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=53 lang=javascript
+ *
+ * [53] 最大子序和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+  // pre保存之前的最大值
+  let pre = 0, maxAns = nums[0];
+  nums.forEach((x) => {
+      pre = Math.max(pre + x, x);
+      maxAns = Math.max(maxAns, pre);
+  });
+  return maxAns;
+};
+console.log(maxSubArray([-2,2,-1,-2,4,-1,2,1,-5,4]));
+// @lc code=end
+
