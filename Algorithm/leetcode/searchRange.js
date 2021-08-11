a2c6ebe1f9474e799df43f3a347bed05053f4b68
LuoMing <Indomite@qq.com>
2021-08-11 20:53:45 +0800

    Delete Algorithm/leetcode directory

--- Algorithm/leetcode/searchRange.js
+++
@@ -1,7 +0,0 @@
-// 34. 在排序数组中查找元素的第一个和最后一个位置
-
-var searchRange = function(nums, target) {
-  return [nums.indexOf(target),nums.lastIndexOf(target)]
-};
-
-console.log(searchRange([5,7,7,8,8,10],8));
